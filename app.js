af0aaae60118ed987196acd0d8c00eebc1c36c40
Kutbeddin <korkmaz4851@gmail.com>
2021-11-30 17:50:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const flexswitch = document.querySelector("#flexswitch");
+const content = document.querySelector(".content");
+
+
+
+
+flexswitch.addEventListener("click", change)
+
+function change(){
+   
+   content.classList.toggle("kut");
+  
+
+    
+}
